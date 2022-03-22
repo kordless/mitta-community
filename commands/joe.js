24683cc6ab1d7bfc63c2f4501aa734cabe798583
Kord Campbell <kordless@gmail.com>
2022-03-21 23:21:02 -0500

    base to joe

--- commands/joe.js
+++ commands/joe.js
@@ -1,1 +1,9 @@
+// joe.js
+// description: the joe command
+// author: kordless@gmail.com
+// copyright: all rights reserved, 2020
+// license: BSD-3-Clause
+
+{{base}}
+
 controller.report(System("Hello Joe!"));
